4aa782f6627363a911568c5aa48f95960b31c104
mdani <mdani@DESKTOP-7SIL5SR>
2025-07-26 21:03:02 -0600

    Proyecto - Parte Daniela

--- src/main/java/com/mycompany/proyecto/RegistroUsuarioMenu.java
+++ src/main/java/com/mycompany/proyecto/RegistroUsuarioMenu.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-//LA PARTE QUE ESTÁ GRIS, NO LA HICE YO
 package com.mycompany.proyecto;
 
 import javax.swing.JOptionPane;
