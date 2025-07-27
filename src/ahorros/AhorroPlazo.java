32794725c9a2df83bd3d08d3d91200bf6deb79de
Isaac-Romero129 <iromero00456@ufide.ac.cr>
2025-07-26 20:24:18 -0600

    Proyecto - Parte Isaac

---
+++ src/ahorros/AhorroPlazo.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ahorros;
+
+import javax.swing.JOptionPane;
+public class AhorroPlazo {
+    private double monto;
+    private int meses;
+    private double tasaInteres;
+    
+    public AhorroPlazo(double monto, int meses, double tasaInteres){
+        this.monto = monto;
+        this.meses = meses;
+        this.tasaInteres = tasaInteres;
+    }
+    public double calcularInteres(){
+        return monto * tasaInteres * meses;
+    }
+    public double montoFinal(){
+        return monto + calcularInteres();
+    }
+    public void mostrarResumen(){
+        String resumen = "Ahorro a plazo\n"
+                + "---------------------\n"
+                + "Monto inicial: " + monto + "\n"
+                + "Meses: " + meses + "\n"
+                + "Tasa de interés mensual: " + (tasaInteres * 100) + "%\n"
+                + "Intereses generados: " + calcularInteres() + "\n"
+                + "Monto final: " + montoFinal();
+        JOptionPane.showMessageDialog(null, resumen, "Resumen de Ahorro a Plazo", JOptionPane.INFORMATION_MESSAGE);
+        
+        
+    }
+    
+}
