2297f384981f8804103b28d43fe0d09076ccf1a7
mdani <mdani@DESKTOP-7SIL5SR>
2025-07-26 18:44:43 -0600

    Proyecto - Parte Daniela

--- src/main/java/com/mycompany/proyecto/Proyecto.java
+++ src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -4,13 +4,59 @@
 
 package com.mycompany.proyecto;
 
-/**
- *
- * @author mdani
- */
-public class Proyecto {
+import javax.swing.JOptionPane;
+
+public class ProyectoPOO {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        while (true) {
+            String opcion = JOptionPane.showInputDialog(
+                "=== MENÚ PRINCIPAL ===\n" +
+                "1. Gestión de Usuarios\n" +
+                "2. Salir\n"
+            );
+            switch (opcion) {
+                case "1":
+                    menuClientes();
+                    break;
+                case "2":
+                    JOptionPane.showMessageDialog(null, "Saliendo del sistema...");
+                    System.exit(0);
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+                    break;
+            }
+        }
+    }
+
+    static void menuClientes() {
+        while (true) {
+            String opcion = JOptionPane.showInputDialog(
+                " == BIENVENIDO AL SISTEMA BANCARIO ==\n" +
+                "1. Iniciar sesión\n" +
+                "2. Registrarse\n" +
+                "3. Registros recientes\n" +                        
+                "4. Salir"
+            );
+
+            switch (opcion) {
+                case "1":
+                    RegistroUsuario.iniciarSesion();
+                    break;
+                case "2":
+                    RegistroUsuario.registrarUsuario();
+                    break;
+                case "3":
+                    RegistroUsuario.mostrarUsuarios();                   
+                    break;
+                case "4":
+                    JOptionPane.showMessageDialog(null, "Saliendo del sistema...");
+                    System.exit(0);
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida.");
+                    break;
+            }
+        }
     }
 }
